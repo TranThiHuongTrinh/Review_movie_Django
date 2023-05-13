f2972b32bb021387056740c079c07052a8dbad8a
Ho Thuy Tien <=>
2023-05-13 14:09:40 +0700

    done account features

--- home/templates/src/assets/js/Account/signIn.js
+++ home/templates/src/assets/js/Account/signIn.js
@@ -1,9 +1,10 @@
 import {submitSignInForm} from './index.js'
 
-const submit_btn = document.querySelector('.signIn')
+const submit_btn = document.querySelector('input[type="submit"]')
     
-submit_btn.addEventListener('click', submitSignInForm, (event) => {
+submit_btn.addEventListener('click', (event) => {
     event.preventDefault();
+    submitSignInForm()
 });
 
 
--- home/templates/src/assets/js/UI/switchForm.js
+++ home/templates/src/assets/js/UI/switchForm.js
@@ -1,7 +1,6 @@
 const headingForm_text = document.querySelector('.form-heading')
 const headingIntroduce_text = document.querySelector('.introduce-heading')
 const submit_btn = document.querySelector('input[type="submit"]')
-const forgetPassword_btn = document.querySelector('.forget-password')
 const switch_btn = document.querySelector('.switch')
 const formContainer = document.querySelector('.form-container')
 const switchContainer = document.querySelector('.switch-container')
@@ -14,28 +13,31 @@ switch_btn.addEventListener('click', function (e) {
     setTimeout(function () {
         name.classList.toggle('hidden')
         retype.classList.toggle('hidden')
-        forgetPassword_btn.classList.toggle('hidden')
     }, 200)
 
     if (isSignIn) {
-        formContainer.classList.remove('translate-x-[384px]')
-        switchContainer.classList.remove('-translate-x-[1152px]')
+        formContainer.classList.remove('translate-x-[370px]')
+        switchContainer.classList.remove('-translate-x-[1111px]')
         setTimeout(function () {
             headingForm_text.innerHTML = "LOGIN TO YOUR ACCOUNT"
             headingIntroduce_text.innerHTML = "NEW HERE ?"
             switch_btn.innerHTML = "Sign Up"
             submit_btn.value = "Sign In"
+            submit_btn.classList.add("signIn")
+            submit_btn.classList.remove("signUp")
         }, 200)
         isSignIn = false
     }
     else {
-        formContainer.classList.add('translate-x-[384px]')
-        switchContainer.classList.add('-translate-x-[1152px]')
+        formContainer.classList.add('translate-x-[370px]')
+        switchContainer.classList.add('-translate-x-[1111px]')
         setTimeout(function () {
             headingForm_text.innerHTML = "CREATE YOUR ACCOUNT"
             headingIntroduce_text.innerHTML = "ALREADY HAVE ?"
             switch_btn.innerHTML = "Sign In"
             submit_btn.value = "Sign Up"
+            submit_btn.classList.remove("signIn")
+            submit_btn.classList.add("signUp")
         }, 200)
         isSignIn = true
     }
