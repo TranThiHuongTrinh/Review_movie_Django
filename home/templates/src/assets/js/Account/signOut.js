54d77638827bad2deb42b35f0f3bb04ec4558fee
Ho Thuy Tien <=>
2023-05-18 13:56:25 +0700

    done project

--- home/templates/src/assets/js/Account/signOut.js
+++
@@ -1,7 +0,0 @@
-import { signOut } from "./index.js";
-
-const signOut_btn = document.querySelector('.signout')
-
-signOut_btn.addEventListener('click', signOut, function (event) {
-    event.preventDefault();
-})
